e1b1bca97b903820839e0f8648a0446e52d2f9c2
Simone Raffoni <SIMONE.RAFFONI@local.ittcesena.it>
2025-10-31 09:41:13 +0100

    Classe CD

--- ProgramCD/CD.cs
+++ ProgramCD/CD.cs
@@ -8,5 +8,28 @@ namespace ProgramCD
 {
     internal class CD
     {
+       
+            private string titolo;
+            private string autore;
+            private List<Brano> listaBrani;
+
+        public CD(string titolo, string autore)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            listaBrani = new List<Brano>(); // Lista Brani
+        }
+
+        public string GetTitolo { get { return titolo; } }
+        public string GetAutore { get { return autore; } }
+
+        public void SetTitolo(string t) { titolo = t; }
+        public void SetAutore(string a) { autore = a; }
+
+        public void AggiungiBrano(Brano  b)
+        {
+            listaBrani.Add(b);
+        }
+
     }
 }

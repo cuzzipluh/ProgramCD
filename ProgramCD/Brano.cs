0ac7ea14da9a21de6f34c6508eee37bc4fb8287c
Marco Ravaioli <MARCO.RAVAIOLI@local.ittcesena.it>
2025-11-07 08:52:23 +0100

    Versione finale

--- ProgramCD/Brano.cs
+++ ProgramCD/Brano.cs
@@ -8,5 +8,51 @@ namespace ProgramCD
 {
     internal class Brano
     {
+        public string Titolo {  get; set; }
+        public string Autore { get; set; }
+        public double Durata { get; set; }
+
+        public string getTitolo()
+        {
+           return Titolo;
+        }
+
+        public string getAutore()
+        {
+            return Autore;
+        }
+
+        public double getDurata()
+        {
+            return Durata;
+        }
+        public void setTitolo(string nuovoTitolo)
+        {
+            Titolo = nuovoTitolo;
+        }
+
+        public void setAutore(string nuovoAutore)
+        {
+            Autore = nuovoAutore;
+        }
+
+        public void setDurata(double nuovaDurata)
+        {
+            if (Durata < 0)
+            {
+                throw new Exception("La durata deve essere maggiore di 0");
+            }
+            Durata = nuovaDurata;
+        }
+
+        public string ToString()
+        {
+            return $"Brano: {Titolo} | Autore: {Autore} | Durata: {Durata}";
+        }
+
+        public bool ShortSong(double maxDurata)
+        {
+            return Durata < maxDurata;
+        }
     }
 }
